8b5d4a7b1e1542fe2523283298980373a1f5b536
RINKUJOVIN <rinku.benny@gmail.com>
2022-07-18 09:25:50 -0400

    read words in a file

---
+++ src/main/java/io/pragra/learning/main/Solution.java
@@ -0,0 +1,28 @@
+package io.pragra.learning.main;
+
+import java.io.File;
+
+/*public class Solution {
+    public static void main(String[] args) {
+
+        //Read files
+        //Define path
+
+        Path path = Paths.get("alice.txt");
+        try{
+            long count =0;
+            List<String>lines = File.readAllLines(path);
+            for (String line:lines){
+                count =count+Arrays.stream(line.split(" ")).filter(w->w.length()>4).count();
+            }
+            System.out.println(count);
+        }catch (IOException ex){
+
+        }
+
+
+
+    }
+}}
+
+ */
